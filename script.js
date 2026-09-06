e5bcd130df669faeb6cd6dc347797961630eef17
hyprlandGOD <lucas.lagrange.2012@gmail.com>
2026-09-06 12:36:02 +0200

    ajout d'un robot tkt et d'une sitemap

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-projets").addEventListener("click", function() {
+    window.location.href = "projets.html";
+});
+
+document.getElementById("btn-contact").addEventListener("click", function() {
+    window.location.href = "contact.html";
+});
